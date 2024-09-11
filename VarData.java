b119d41073648fa8b9bb031cc3e263e62f937b75
JChan5052 <jeffreyc172@nycstudents.net>
2024-09-11 03:09:50 +0000

    first commit

---
+++ VarData.java
@@ -0,0 +1,21 @@
+public class VarData
+{
+    public static void main(String[] args)
+    {
+        int num = 1;
+        double num2 = 2;
+        int num3;
+        double num4;
+        boolean check1 = true;
+        String chars = "false";
+        final int num5 = 3;
+        System.out.println(num + num2);
+        num3 = num + 1;
+        num4 = num2 +1;
+        System.out.println(num3);
+        System.out.println(num4);
+        System.out.println(num5);
+        System.out.println(chars);
+        System.out.println(check1);
+    }
+}
